bcd878aaec1a90e4ade9a5fbfeb5f15deb3b69a4
Marco Milani <Marco Milani>
2024-12-18 10:03:23 +0100

    Project complete v.1

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -10,4 +10,4 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:pgsu2024@localhost:5432/fyyur'
